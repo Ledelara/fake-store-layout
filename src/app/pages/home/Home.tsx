2476e2ac1d5ae70e90de72f4616e67190e2ef35d
Leandro de Lara Celio <122060742+Ledelara@users.noreply.github.com>
2024-12-15 11:09:01 -0300

    ♻️ Refactor: chamando BestSellers na Home

--- src/app/pages/home/Home.tsx
+++ src/app/pages/home/Home.tsx
@@ -1,40 +1,11 @@
 'use client'
-
-import { useQuery } from "@tanstack/react-query";
-import { getProducts } from "../../services/api";
-import { Product } from "../../interface/interface";
-import ProductCard from "../../components/ProductCard/ProductCard";
-import Loading from "../../components/Status/Loading/Loading";
-import Error from "../../components/Status/Error/Error";
+import BestSellers from "../../components/Showcases/BestSellers/BestSellers";
 
 export default function Home() {
-    const { data, isLoading, isError } = useQuery({ 
-        queryKey: ['products'], 
-        queryFn: getProducts,
-    });
 
     return (
         <div className="Home">
-            <h1>Produtos</h1>
-            <ul>
-                {isLoading && (
-                    <Loading 
-                        message="Carregando..." 
-                        isLoading={isLoading} 
-                    />
-                )}
-                {isError && (
-                    <Error 
-                        message="Erro ao carregar produtos" 
-                        isError={isError} 
-                    />
-                )}
-                {data?.map((product: Product) => (
-                    <li key={product.id}>
-                        <ProductCard product={product} />
-                    </li>
-                ))}
-            </ul>
+            <BestSellers />
         </div>
     );
 }
